3acd876168cdf5ef7cd7c7196504363443a769e5
lyl <419314472@qq.com>
2019-09-28 22:39:13 +0800

    分段缓存，解决内存占用过大的问题

--- utils/config.py
+++ utils/config.py
@@ -4,13 +4,18 @@
 #@Author: 林先森
 #@File  : config.py
 
-DATA_PATH = 'D:\\programming\\PycharmProjects\\ComputerVision\\DataBase\\hefei_ECG\\' # 数据集总目录
+DATA_PATH = 'D:\\良目\\天池大赛\\dataSet\\' # 数据集总目录
 ARRYTHMIA_CLASSES_PATH = 'hf_round1_arrythmia.txt' # 分类文件路径
 LABEL_PATH = 'hf_round1_label.txt' # 标签文件路径
 TRAIN_PATH = 'train\\' # 训练集目录
+CACHE_PATH = 'cache\\' # 缓存文件夹
+BATCH_SIZE = 10
+CACHE_SIZE = 100*BATCH_SIZE #单个缓存文件的大小
+READ_STEP  = 5*CACHE_SIZE #单步读取量
 
 LENGTH = 5000  # 数据十秒内记录的次数
 LEAD_COUNT = 8 # 导联数，默认按
+
 
 
 
