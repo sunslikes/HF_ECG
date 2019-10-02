798ddbf4f560fdff5e4279fd21fddedca2f5acf0
lyl <419314472@qq.com>
2019-10-03 00:59:04 +0800

    模型加入resnet34，可以跑通

--- models/simpleModel.py
+++ models/simpleModel.py
@@ -2,6 +2,7 @@ import numpy as np
 import tensorflow as tf
 from utils import config
 from tensorflow.contrib import slim
+from models.Resnet34 import inference
 class SimpleModel:
      def __init__(self,is_training = True):
          self.lead_count = config.LEAD_COUNT # 导联数
@@ -20,26 +21,31 @@ class SimpleModel:
                  tf.float32,[None,self.label_num]
              )
              self.loss = self.get_loss(self.logits,self.labels) # 函数未构建
+
      """
      构建网络
      """
      def build_network(self,input,output_num,is_training):
-         net = slim.flatten(input)
-         net = slim.fully_connected(net,8000)
-         net = slim.fully_connected(net, 2048)
-         net = slim.fully_connected(net,512)
+         net = input
+         net = tf.reshape(net,[self.batch_size,self.lead_count,self.length,1]) # 将输入变成符合conv2d输入的shape
+         net = inference(net) # ResNet34 的卷积层（去掉最后一层池化）
+         net = slim.flatten(net)
+         net = slim.fully_connected(net, 1024)
+         net = slim.fully_connected(net,256)
          net = slim.fully_connected(net,output_num)
          return net
      """
         将输出的一维向量变成onehot
      """
      def map2OneHot(self,logits):
-        #list = []
+        # list = []
         logits = tf.sigmoid(logits) # 将模型输出的结果通入sigmoid函数
-        #list.append(logits)
-        # one = tf.ones_like(logits)
-        # zero = tf.zeros_like(logits)
-        # logits= tf.where(logits < self.threshold, x=zero, y=one) # 将元素二值化映射，大于threshold的设置为1，反之，0
+        # list.append(logits)
+        one = tf.ones_like(logits)
+        zero = tf.zeros_like(logits)
+        temp = tf.where(logits < self.threshold, x=zero, y=one) # 将元素二值化映射，大于threshold的设置为1，反之，0
+        logits = tf.subtract(logits,logits) # 将图清零
+        logits = tf.add(logits,temp) # 将映射结果加入（为了获取梯度，不知道有没有更好的方法# ）
         # list.append(logits)
         return logits
      """
@@ -50,5 +56,6 @@ class SimpleModel:
          out = -tf.reduce_mean(labels*tf.log(tf.clip_by_value(logits,1e-10,1.0)))
          # return tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=labels))
          return out
+
 if __name__ == '__main__':
     model = SimpleModel(True)
