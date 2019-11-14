7bedc126fe73514aebd655e8c94c8feae6f3f6b9
sunslikes <893222871@qq,com>
2019-11-14 23:30:03 +0800

    備份

--- utils/config.py
+++ utils/config.py
@@ -7,11 +7,15 @@ import os
 
 MODEL_NAME = '' # 模型名称 用于保存
 
-DATA_PATH = 'D:/良目/天池大赛/dataSet/' # 数据集总目录
+DATA_PATH = 'C:/Users/Admin/PycharmProjects/HF_ECG/dataSet/' # 数据集总目录
 ARRYTHMIA_CLASSES_PATH = 'hf_round1_arrythmia.txt' # 分类文件路径
 LABEL_PATH = 'hf_round1_label.txt' # 标签文件路径
 TRAIN_PATH = 'train/' # 训练集目录（需要手动创建）
 CACHE_PATH = 'cache/' # 缓存文件夹（需要手动创建）
+TEST_PATH = 'test\\testA\\' # 测试集文件夹
+TEST_CACHE = 'test\\cache\\'
+ANSWAR_PATH = 'test\\hf_round1_subA.txt' # 试卷
+ANSWERS_DIR = 'test\\answers\\' # 测试结果目录
 WEIGHTS_DIR = os.path.join(DATA_PATH,'weights') # 模型文件夹
 WEIGHTS_FILE = None # 迁移模型
 # WEIGHTS_FILE = 'D:\\良目\\天池大赛\\dataSet\\output\\2019_10_04_23_36\\ECG-100'                          # 迁移模型
@@ -32,9 +36,9 @@ DECAY_RATE = 0.1 # 衰退率
 STAIRCASE = True # 若为true，每DECAY_STEPS次迭代更新学习率，反之，每次迭代都更新
 THRESHOLD = 0.5 # sigmoid函数出来超过这个值将映射为1
 
-SUMMARY_ITER = 10 # 每训练SUMMARY_ITER个批次进行一次评估（计算loss，输出log）
-SAVE_ITER = 500  # 每训练SAVE_ITER个批次进行模型的保存
-MAX_ITER = 2000   # 训练批次
+SUMMARY_ITER = 100 # 每训练SUMMARY_ITER个批次进行一次评估（计算loss，输出log）
+SAVE_ITER = 1000  # 每训练SAVE_ITER个批次进行模型的保存
+MAX_ITER = 20000   # 训练批次
 
 
 
